8d380fa131e328e25d419c2ccce776f25ab906ba
Harry Nelken <harry.nelken@accenture.com>
2020-03-09 14:55:05 -0400

    Initialize repo with project and gitignore

---
+++ Fetch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fetch
+//
+//  Created by Harry Nelken on 3/9/20.
+//  Copyright © 2020 Digital Products. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
