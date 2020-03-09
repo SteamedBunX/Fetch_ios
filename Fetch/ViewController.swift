f997e656d580f038fc0c9a322d0818b5d2208b70
Harry Nelken <harry.nelken@accenture.com>
2020-03-09 15:18:41 -0400

    Setup basic first view controller

--- Fetch/ViewController.swift
+++ Fetch/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    init() {
+        super.init(nibName: String(describing: ViewController.self), bundle: nil)
     }
 
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
-
