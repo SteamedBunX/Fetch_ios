628ed973590337d57f4226424e57be951de9803c
Hao, Yi <yi.hao@accenture.com>
2020-03-19 16:42:58 -0500

    [FETCH-127] Add empty test for building purposes. Will fill up in the future.

---
+++ FetchTests/OnboardingQuestionTests.swift
@@ -0,0 +1,14 @@
+//
+//  FetchTests.swift
+//  FetchTests
+//
+//  Created by Harry Nelken on 3/9/20.
+//  Copyright © 2020 Digital Products. All rights reserved.
+//
+
+import XCTest
+@testable import Fetch
+
+class OnboardingQuestionTests: XCTestCase {
+
+}
