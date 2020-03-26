7ebcbef38e836630bad3d5e6e883f7fd66132dc6
Hao, Yi <yi.hao@accenture.com>
2020-03-26 14:00:04 -0500

    [FETCH-112] Add Test Cases for the QuestionModels.

---
+++ FetchTests/SingleChoiceOnboardingQuestionTests.swift
@@ -0,0 +1,71 @@
+//
+//  TextSingleChoiceOnboardingQuestionTests.swift
+//  FetchTests
+//
+//  Created by yi.hao on 3/26/20.
+//  Copyright © 2020 Digital Products. All rights reserved.
+//
+
+import XCTest
+@testable import Fetch
+
+class SingleChoiceOnboardingQuestionTests: XCTestCase {
+
+    func loadSingleChoiceQuestion() -> OnboardingQuestion {
+        return OnboardingQuestion(
+            title: "When it comes to taking care of pets, I’m",
+            questionType: .singleChoice,
+            choices: ["An Expert",
+                      "Experienced",
+                      "A Novice"])
+    }
+
+    func testWithoutInput() {
+        let sut = loadSingleChoiceQuestion()
+        XCTAssertFalse(sut.isAnswered)
+    }
+
+    func testUserChooseOne() {
+        let sut = loadSingleChoiceQuestion()
+        sut.selectChoice(at: 0)
+        XCTAssertEqual(sut.selectedIndexes, [0])
+        XCTAssertTrue(sut.isAnswered)
+    }
+
+    func testUserDoubleTap() {
+        let sut = loadSingleChoiceQuestion()
+        sut.selectChoice(at: 0)
+        XCTAssertEqual(sut.selectedIndexes, [0])
+        XCTAssertTrue(sut.isAnswered)
+        sut.selectChoice(at: 0)
+        XCTAssertEqual(sut.selectedIndexes, [0])
+        XCTAssertTrue(sut.isAnswered)
+    }
+
+    func testUserChoiceOutOfBound() {
+        let sut = loadSingleChoiceQuestion()
+        sut.selectChoice(at: 5)
+        XCTAssertEqual(sut.selectedIndexes, [])
+        XCTAssertFalse(sut.isAnswered)
+    }
+
+    func testUserChoiceOutOfBoundAfterAnswer() {
+        let sut = loadSingleChoiceQuestion()
+        sut.selectChoice(at: 0)
+        XCTAssertEqual(sut.selectedIndexes, [0])
+        XCTAssertTrue(sut.isAnswered)
+        sut.selectChoice(at: 5)
+        XCTAssertEqual(sut.selectedIndexes, [0])
+        XCTAssertTrue(sut.isAnswered)
+    }
+
+    func testUserChangeChoice() {
+        let sut = loadSingleChoiceQuestion()
+        sut.selectChoice(at: 0)
+        XCTAssertEqual(sut.selectedIndexes, [0])
+        XCTAssertTrue(sut.isAnswered)
+        sut.selectChoice(at: 2)
+        XCTAssertEqual(sut.selectedIndexes, [2])
+        XCTAssertTrue(sut.isAnswered)
+    }
+}
---
+++ FetchTests/TextInputOnboardingQuestionTests.swift
@@ -0,0 +1,65 @@
+//
+//  FetchTests.swift
+//  FetchTests
+//
+//  Created by Harry Nelken on 3/9/20.
+//  Copyright © 2020 Digital Products. All rights reserved.
+//
+
+import XCTest
+@testable import Fetch
+
+class TextInputOnboardingQuestionTests: XCTestCase {
+
+    func loadTextInputQuestion() -> OnboardingQuestion {
+        return OnboardingQuestion(
+            title: "My zip code is",
+            minInputLength: 5,
+            maxInputLength: 10,
+            placeHolderText: "Enter your zip code here",
+            inputKeyboardType: .digit)
+    }
+
+    func testWithoutInput() {
+        let sut = loadTextInputQuestion()
+        XCTAssertFalse(sut.isAnswered)
+    }
+
+    func testWithShorterThanMinInput() {
+        let sut = loadTextInputQuestion()
+        sut.setInputText("1234")
+        XCTAssertFalse(sut.isAnswered)
+    }
+
+    func testWithEqualToMinInput() {
+        let sut = loadTextInputQuestion()
+        sut.setInputText("12344")
+        XCTAssertTrue(sut.isAnswered)
+    }
+
+    func testWithMoreThanMinLessThanMaxInput() {
+        let sut = loadTextInputQuestion()
+        sut.setInputText("12345678")
+        XCTAssertTrue(sut.isAnswered)
+    }
+
+    func testWithEqualToMaxInput() {
+        let sut = loadTextInputQuestion()
+        sut.setInputText("1234567890")
+        XCTAssertTrue(sut.isAnswered)
+    }
+
+    func testWithMoreThanMaxInput() {
+        let sut = loadTextInputQuestion()
+        sut.setInputText("1234567890123")
+        XCTAssertFalse(sut.isAnswered)
+    }
+
+    func testAnswerThanNotAnswered() {
+        let sut = loadTextInputQuestion()
+        sut.setInputText("12345678")
+        XCTAssertTrue(sut.isAnswered)
+        sut.setInputText("1234")
+        XCTAssertFalse(sut.isAnswered)
+    }
+}
