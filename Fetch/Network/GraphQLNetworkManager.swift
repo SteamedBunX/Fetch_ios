10d6f46fd7ec3cafda7d65243f6243444e9fc434
Hao, Yi <yi.hao@accenture.com>
2020-04-09 08:59:37 -0500

    [FETCH-17] Add GraphQLNetworkManager class.

---
+++ Fetch/Network/GraphQLNetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  GraphQLNetworkManager.swift
+//  Fetch
+//
+//  Created by yi.hao on 4/9/20.
+//  Copyright © 2020 Digital Products. All rights reserved.
+//
+
+import Foundation
+import Apollo
+
+final class GraphQLNetworkManager {
+    
+}
